2af9a35ce0e41dff2f5da8409db7a9eaab7a3e11
Ed Burns <edburns@microsoft.com>
2024-09-26 13:46:23 -0700

    On branch edburns-msft-em-6189-synchronize-azure.liberty.aks-versions Source env script and checkout specified version. modified:   azd-hooks/preprovision.sh modified:   .scripts/setup-env-variables-template.sh
    Signed-off-by: Ed Burns <edburns@microsoft.com>

--- .scripts/setup-env-variables-template.sh
+++ .scripts/setup-env-variables-template.sh
@@ -8,6 +8,6 @@ export DB_PASSWORD="Secret123456" # PostgreSQL database password
 export DB_PORT_NUMBER=5432
 export DB_SERVER_NAME="${DB_RESOURCE_NAME}.postgres.database.azure.com" # PostgreSQL host name
 export DB_USER=liberty${DB_RESOURCE_NAME}
-export LIBERTY_AKS_REPO_REF="048e776e9efe2ffed8368812e198c1007ba94b2c" # WASdev/azure.liberty.aks
+export LIBERTY_AKS_REPO_REF="5886de1248e1cdcc891c1135d6ad3ae6660f0adf" # WASdev/azure.liberty.aks
 export NAMESPACE=default
 export WORKSPACE_NAME="${RESOURCE_GROUP_NAME}ws"
